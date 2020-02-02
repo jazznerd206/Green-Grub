16318d4b0fa53882aed7b074890f3f40d1fefd70
Dima <dima.dibb1@gmail.com>
2020-02-01 22:50:11 -0800

    React router

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NotLoggedIn from "./pages/NotLoggedIn";
 import "./App.css";
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={NotLoggedIn} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
