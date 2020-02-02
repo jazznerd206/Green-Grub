082f1cb4a6e21b4df7085e2db4f078891ffb271e
Andrew Miller <206alm@gmail.com>
2020-02-02 10:59:51 -0800

    semantic change

--- client/src/components/LoggedIn/LoggedIn.js
+++ client/src/components/LoggedIn/LoggedIn.js
@@ -4,44 +4,50 @@ import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 
 
-class Dashboard extends Component {
+class LoggedIn extends Component {
+
+  componentDidMount() {
+    console.log(this.props.auth)
+  }
+  
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
   };
-render() {
-  const { user } = this.props.auth;
-    return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                You are logged into a full-stack{" "}
-                <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
+
+  render() {
+    const { user } = this.props.auth;
+      return (
+        <div style={{ height: "75vh" }} className="container valign-wrapper">
+          <div className="row">
+            <div className="col s12 center-align">
+              <h4>
+                <b>Hey there,</b> {user.name.split(" ")[0]}
+                <p className="flow-text grey-text text-darken-1">
+                  You are logged into a full-stack{" "}
+                  <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
+                </p>
+              </h4>
+              <button
+                style={{
+                  width: "150px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px",
+                  marginTop: "1rem"
+                }}
+                onClick={this.onLogoutClick}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Logout
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
 
-Dashboard.propTypes = {
+LoggedIn.propTypes = {
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
 };
@@ -53,4 +59,4 @@ const mapStateToProps = state => ({
 export default connect(
   mapStateToProps,
   { logoutUser }
-)(Dashboard);
+)(LoggedIn);
