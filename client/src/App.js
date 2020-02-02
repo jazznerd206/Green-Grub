cbd4c76a92b3f4e9f5c737115ae5cb442548fc9f
Andrew Miller <206alm@gmail.com>
2020-02-02 08:57:57 -0800

    auth package in front end, need to connect it to back end now

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home"
+//import Home from "./pages/Home"
+import Nav from "./components/Layout/Navbar";
 import NotLoggedIn from "./pages/NotLoggedIn";
+import Login from "./components/auth/Login.js";
+import Register from "./components/auth/Register.js";
 import "./App.css";
-import Nav from "./components/Navbar";
-import Construction from "./components/Construction";
+//import Construction from "./components/Construction";
 
-function App() {
-  return (
-    <Router>
-      <Nav />
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/notloggedin" component={NotLoggedIn} />
-          </Switch>
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Nav />
+          <Route exact path="/" component={NotLoggedIn} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
         </div>
-      <Construction />
-    </Router>
-  );
+      </Router>
+    );
+  }
 }
-
 export default App;
---
+++ client/src/components/Layout/Navbar/index.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Container, Navbar, NavItem } from 'react-materialize';
+
+
+class Nav extends Component {
+  render() {
+    return (
+      <Container>
+        <Navbar
+          alignLinks="right"
+          brand={<a className="brand-logo" href="#">Green Grub</a>}
+          options={{
+            draggable: true,
+            edge: 'left',
+            inDuration: 250,
+            onCloseEnd: null,
+            onCloseStart: null,
+            onOpenEnd: null,
+            onOpenStart: null,
+            outDuration: 200,
+            preventScrolling: true
+          }}
+        >
+          <NavItem href="">
+            <Link
+                  to="/login"
+                  style={{
+                    width: "140px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px"
+                  }}
+                  className="btn btn-large btn-flat waves-effect white black-text"
+                >
+                  Log In
+            </Link>
+          </NavItem>
+          <NavItem href="components.html">
+            <Link
+                to="/register"
+                style={{
+                  width: "140px",
+                  borderRadius: "3px",
+                  letterSpacing: "1.5px"
+                }}
+                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+              >
+                Register
+            </Link>
+          </NavItem>
+        </Navbar>
+      </Container>
+    );
+  }
+}
+export default Nav;
