8081b139870368a18080d6be3b29749d52dab73b
Dima <dima.dibb1@gmail.com>
2020-02-01 22:50:11 -0800

    notloggedin

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home"
+import NotLoggedIn from "./pages/NotLoggedIn";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Construction from "./components/Construction";
-
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <div>
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/notloggedin" component={NotLoggedIn} />
           </Switch>
         </div>
     </Router>
