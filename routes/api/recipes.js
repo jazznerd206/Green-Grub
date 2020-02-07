66d521d69598274491610fadef8400a872e40986
Jeff Huang <jeffshuang2@gmail.com>
2020-02-06 19:14:58 -0800

    Created CRUD operations for recipes collection.

---
+++ routes/api/recipes.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const recipesController = require("../../controllers/recipesController");
+
+router.route("/recipes")
+    .get(recipesController.findAll)
+
+router.route("/recipesDone")
+    .get(recipesController.findRecipesDone)
+
+router.route("/recipesTodo")
+    .get(recipesController.findRecipesTodo)
+
+router.route("/markRecipeDone")
+    .put(recipesController.markRecipeDone)
+
+router.route("/markRecipeTodo")
+    .put(recipesController.markRecipeTodo)
