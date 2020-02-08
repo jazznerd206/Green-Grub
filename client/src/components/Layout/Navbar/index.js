adfef96bba40640b64b540ac9ef1ce8d2fb2290f
Dima <dima.dibb1@gmail.com>
2020-02-07 22:23:55 -0800

    fix navbar style

--- client/src/components/Layout/Navbar/index.js
+++ client/src/components/Layout/Navbar/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import { Container, Navbar, NavItem, Button, Col } from 'react-materialize';
+import { Container, Navbar, NavItem, Button, Row, Col } from 'react-materialize';
 import "./style.css";
 
 
@@ -13,48 +13,23 @@ class Nav extends Component {
         <Navbar className="navbar-color-design"
           alignLinks="right"
           brand={<a className="navbar" href="#">Green Grub</a>}
-          // options={{
-          //   draggable: true,
-          //   edge: 'left',
-          //   inDuration: 250,
-          //   onCloseEnd: null,
-          //   onCloseStart: null,
-          //   onOpenEnd: null,
-          //   onOpenStart: null,
-          //   outDuration: 200,
-          //   preventScrolling: true
-          // }}
-
+          options={{
+            draggable: true,
+            edge: 'left',
+            inDuration: 250,
+            onCloseEnd: null,
+            onCloseStart: null,
+            onOpenEnd: null,
+            onOpenStart: null,
+            outDuration: 200,
+            preventScrolling: true
+          }}
         >
-          <NavItem href="">
-          <Col className="color">
-              <Button node="button"style={{ marginRight: '5px'}} waves ="light">
-                <Link
-                  to="/login"
-                  style={{
-                    width: "140px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px"
-                  }}
-                  className="btn btn-large btn-flat waves-effect white black-text"
-                >
-                  Log In
-                </Link>
-              </Button>
-              <Button  node="button"style={{ marginRight: '5px' }} waves ="light">
-                <Link
-                to="/register"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register
-                </Link>
-              </Button>
-            </Col>
+          <NavItem href="/login">
+            Log In
+          </NavItem>
+          <NavItem href="/register">
+            Register
           </NavItem>
         </Navbar>
       </Container>
