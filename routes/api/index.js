15b9a9a77f0cc6109e234b2ae21436204bb75a8c
Jeff Huang <jeffshuang2@gmail.com>
2020-02-15 14:22:37 -0800

    Andrew's commit.

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,12 +3,12 @@ const articlesRoutes = require("./articles");
 const blogsRoutes = require("./blogs");
 const recipesRoutes = require("./recipes");
 const usersRoutes = require("./users");
+console.log("indexapi");
 
 // Book routes
 router.use("/articles", articlesRoutes);
 router.use("/blogs", blogsRoutes);
 router.use("/recipes", recipesRoutes);
 router.use("/users", usersRoutes);
-
 
 module.exports = router;
