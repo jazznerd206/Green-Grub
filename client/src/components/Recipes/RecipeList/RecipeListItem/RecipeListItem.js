3daa00f5a39b58e23d61dffc89c281d02e2f0faf
Dima <dima.dibb1@gmail.com>
2020-02-17 22:13:36 -0800

    Recipe

---
+++ client/src/components/Recipes/RecipeList/RecipeListItem/RecipeListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Container, Row, Col, Button } from "react-materialize";
+import "./style.css";
+
+function RecipeListItem(props) {
+  return (
+    <Container className="recipe-container">
+      <Row>
+        <Col>
+          <h4>{props.recipe.title}</h4>
+        </Col>
+      </Row>
+      <Row>
+        <Col>Servings: {props.recipe.servings}</Col>
+        <Col>Time in minutes: {props.recipe.readyInMinutes}</Col>
+        <Col className="ml-auto"><Button>Save</Button></Col>
+      </Row>
+      <Row>
+        <Col s={3}>
+          <img src={props.recipe.image} className="recipe-image"/>
+        </Col>
+        <Col s={9}>
+          <h5>Instructions:</h5>
+          <span>{props.recipe.instructions}</span>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default RecipeListItem;
