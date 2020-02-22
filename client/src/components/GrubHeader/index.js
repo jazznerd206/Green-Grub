a01cad0724aa6935fd1fd591d392468a3cde8cee
Andrew Miller <206alm@gmail.com>
2020-02-22 09:42:43 -0800

    updated jumbotron to add background to text

--- client/src/components/GrubHeader/index.js
+++ client/src/components/GrubHeader/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Slider, Slide, Caption} from "react-materialize"
+import "./style.css";
 
 export default function GrubHeader() {
     return (
@@ -16,40 +17,40 @@ export default function GrubHeader() {
             >
             <Slide image={<img alt="" src="https://images.unsplash.com/photo-1490818387583-1baba5e638af?ixlib=rb-1.2.1&dpr=2&auto=format&fit=crop&w=416&h=312&q=60"/>}>
                 <Caption placement="center">
-                <h3 className="light grey-text text-darken-3">
+                <h3 className="caption-container light grey-text text-darken-5">
                     Big taste, bigger reward.
                 </h3>
-                <h5 className="light grey-text text-darken-3">
+                <h5 className="caption-container light grey-text text-darken-5">
                     Here's to your health!
                 </h5>
                 </Caption>
             </Slide>
             <Slide image={<img alt="" src="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"/>}>
-                <Caption placement="left">
-                <h3 className="light grey-text text-lighten-3">
+                <Caption placement="left" >
+                <h3 className="caption-container light grey-text text-lighten-3">
                     Healthy can be delicious.
                 </h3>
-                <h5 className="light grey-text text-lighten-3">
+                <h5 className="caption-container light grey-text text-lighten-3">
                     No compromise, full flavor.
                 </h5>
                 </Caption>
             </Slide>
             <Slide image={<img alt="" src="https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"/>}>
                 <Caption placement="right">
-                <h3>
+                <h3 className="caption-container light grey-text text-lighten-3">
                     Hungry? Search for a recipe.
                 </h3>
-                <h5 className="light grey-text text-lighten-3">
+                <h5 className="caption-container light grey-text text-lighten-3">
                     Still hungry? Check your saved recipes for another idea...
                 </h5>
                 </Caption>
             </Slide>
             <Slide image={<img alt="" src="https://images.unsplash.com/photo-1543362906-acfc16c67564?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=60"/>}>
                 <Caption placement="center">
-                <h3>
+                <h3 className="caption-container light grey-text text-lighten-3">
                     Feed your body, feed your mind!
                 </h3>
-                <h5 className="light grey-text text-lighten-3">
+                <h5 className="caption-container light grey-text text-lighten-3">
                     Diet:Physical Health :: Knowledge:Mental Health
                 </h5>
                 </Caption>
